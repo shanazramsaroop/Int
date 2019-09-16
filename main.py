826758d6d470a393e01542a00beb90c4abc0ae29
shanazramsaroop <ssramsaroop6835@eagle.fgcu.edu>
2019-09-16 16:05:33 -0400

    added code

---
+++ main.py
@@ -0,0 +1,6 @@
+#Shanaz Ramsaroop
+#This is a fun trvia game
+print("Hello, welcome to my trivia game. Hope you have fun!")
+answer1=input("What is the remainder if 103 is divided by 5")
+correctAnswer1=103 % 5
+print(answer1==correctAnswer1)
