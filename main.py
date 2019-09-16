bb9cadeefeb814c915c7a4075dfdd9d2e0441da6
shanazramsaroop <ssramsaroop6835@eagle.fgcu.edu>
2019-09-16 16:18:52 -0400

    assigned answer1 to int

--- main.py
+++ main.py
@@ -2,5 +2,6 @@
 #This is a fun trvia game
 print("Hello, welcome to my trivia game. Hope you have fun!")
 answer1=input("What is the remainder if 103 is divided by 5")
+ans1=int(answer1)
 correctAnswer1=103 % 5
-print(answer1==correctAnswer1)
+print(ans1==correctAnswer1)
