0690b0c488ca6698cf8dda453aef8d65ba8d509e
shanazramsaroop <ssramsaroop6835@eagle.fgcu.edu>
2019-10-13 00:16:40 -0400

    added if else statement

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #Shanaz Ramsaroop
 #This is a fun trvia game
-print("Hello, welcome to my trivia game. Hope you have fun!")
-answer1=int(input("What is the remainder if 103 is divided by 5? Answer = "))
+
+print("Hello, welcome to my exciting trivia game. Hope you have fun!")
+print("Number 1: What is the remainder if 103 is divided by 5?")
+answer1=int(input("Your answer = "))
 correctAnswer1=(103 % 5)
-print(answer1==correctAnswer1)
+if answer1==correctAnswer1:
+    print("Correct! You are so smart!")
+else:
+    print("Oops! Not quite right.")
+
+print("Number 2: ")
+answer2=int(input())
