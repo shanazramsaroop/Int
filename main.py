f86acccffcd6c993059d84dad6b65b1bcbe14ba4
shanazramsaroop <ssramsaroop6835@eagle.fgcu.edu>
2019-10-25 13:27:02 -0400

    added tryagain functions

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 #Shanaz Ramsaroop
-#This is program with number games
+#This is a program with number games
 
+
+def tryAgain1():
+    print("Enter 1 to try again. ")
+    print("Enter 2 to select another game. ")
+    print("")
+    var = int(input())
+    if var == 2:
+        main()
+    elif var == 1:
+        number(1)
+
+def tryAgain3():
+    print("Enter 1 to try again. ")
+    print("Enter 2 to select another game. ")
+    print("")
+    var = int(input())
+    if var == 2:
+        main()
+    elif var == 1:
+        number(3)
 
 def number1(answer1):
     correctAnswer1 = (103 % 5)
     while answer1 != correctAnswer1:
         print("Oops! Not quite right. ")
-        print("Enter 1 to try again. ")
-        print("Enter 2 to select another game. ")
-        selection = int(input())
-        if selection == 1:
-             answer1 = int(input("Your answer = "))
+        print("")
+        tryAgain1()
 
-    print("Correct! You are so smart. ")
+    print("Good job! You got it correct. ")
 
 
 def number2(a, b):
@@ -36,47 +53,69 @@ def number3(guess):
         elif guess < 1 or guess > 20:
             print("Invalid number. Try again. ")
 
+        attempts = 5 - guesses
+
+        while guesses > 5:
+            print("Sorry, you ran out of attempts.")
+            tryAgain3()
+        print("Number of attempts left =", attempts)
         guess = int(input("Guess any number between 1 and 20"))
 
-    print("Correct, it took you", guesses, "guesses.")
+    if guesses == 0:
+        print("You got it on the first try, good guess!")
+
+    elif guesses == 1:
+        print("Correct, it took you just one guess!")
+
+    elif guesses <= 5:
+        print("Correct, it took you", guesses, "guesses.")
 
 
 def number(result):
 
     #option 1
     if result == 1:
+        print("")
         print("Math question!")
         print("What is the remainder if 103 is divided by 5?")
         answer1 = int(input("Your answer = "))
+        print("")
         number1(answer1)
         main()
 
     #option 2
     elif result == 2:
+        print("")
         print("Print numbers!")
         print("Enter any  2 numbers")
         line1 = int(input("First number: "))
         line2 = int(input("Second number: "))
+        print("")
         number2(line1, line2)
 
         main()
 
     #option 3
     elif result == 3:
+        print("")
         print("Guessing game!")
         guess = int(input("Guess any number between 1 and 20"))
+        print("")
         number3(guess)
         main()
 
    #invalid number
     elif result > 4 or result < 1:
+        print("")
         print("Invalid number. Please enter a valid number. ")
+        print("")
         main()
 
 
 def main():
     print("")
     print("Welcome to my number game!")
+    print("")
     print("Choose an option: ")
     print(" 1) Math question ")
     print(" 2) Print numbers ")
